12a1651b59b46cd6a1e7e500cb901ac5afd1737e
CarBen07 <carlosangenlbenitez07@gmail.com>
2024-07-10 02:16:53 -0300

    Primer Base de Datos

---
+++ js/fetch_post.js
@@ -0,0 +1,24 @@
+let submitButton = document.querySelector("#contactForm #Crear");
+
+submitButton.addEventListener("click", ()=>{
+    let data_post = {
+        'nombre': document.querySelector("#contactForm #nombre").value,
+        'email': document.querySelector("#contactForm #email").value,
+        'telefono': document.querySelector("#contactForm #telefono").value,
+        'asunto': document.querySelector("#contactForm #asunto").value,
+        'mensaje': document.querySelector("#contactForm #mensaje").value
+        
+    }
+    
+    fetchData(
+        "http://localhost:5000/api/client/create/",
+        "POST",
+        (data) => {
+            document.querySelector("#Formulario").reset();
+            //window.location.replace("../index.html#TareasPendientes");
+        },
+        data_post
+    );
+    }
+);
+
